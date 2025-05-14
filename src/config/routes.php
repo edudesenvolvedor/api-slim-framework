942268b2aa183c5273a3d3dcffc5b976e2e3f989
Eduardo Pacheco <epacheco.dev@gmail.com>
2025-05-14 19:36:13 -0300

    feat: add slim framework

---
+++ src/config/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response, $args) {
+  $response->getBody()->write("Hello, World!");
+  return $response;
+});
+
+$app->run();
